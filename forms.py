8573fb1d8b460c84a66516e8fbac9ebd78882b66
maxim <maximpely@gmail.com>
2018-04-12 19:55:30 +0100

    change websettings to a tick box

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, BooleanField
 from wtforms.validators import DataRequired
 
 
@@ -10,4 +10,4 @@ class AlbumSearch(FlaskForm):
 class SettingsForm(FlaskForm):
     path = StringField('path', validators=[DataRequired()])
     command = StringField('command', validators=[DataRequired()])
-    websettings = StringField('websettings', validators=[DataRequired()])
+    websettings = BooleanField('websettings')
