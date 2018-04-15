24907f93d3a211ec777535a90122ad3e8e1912ca
maxim <maximpely@gmail.com>
2018-04-15 13:35:00 +0100

    fixed external execute returning you to home page

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, redirect, flash
+from flask import Flask, render_template, url_for, redirect, flash, request
 
 from deezer import search, execute, set_settings, get_settings
 from forms import AlbumSearch, SettingsForm
@@ -12,32 +12,36 @@ def index():
     form = AlbumSearch()
     if form.validate_on_submit():
         return redirect(
-            url_for('result', search_type=str(form.search_type.data), search_term=str(form.search_term.data)))
+            url_for('search_result', search_type=str(form.search_type.data), search_term=str(form.search_term.data)))
     return render_template('index.html', form=form)
 
 
-@app.route('/result/<search_type>/<search_term>')
-def result(search_type, search_term):
+@app.route('/search_result/<search_type>/<search_term>')
+def search_result(search_type, search_term):
     if not (search_term and search_type):
         flash('You need to search for something before we can show results')
         return redirect(url_for('index'))
     data = search(search_type, search_term)
     if data:
-        return render_template('result.html', term=search_term, data=data)
+        return render_template('search_result.html', term=search_term, data=data)
     else:
         flash('No data was returned for that search term')
         return redirect(url_for('index'))
+
+
+@app.route('/tracklist/<name>/<id>')
+def tracklist(name, id):
+    pass
 
 
 @app.route('/get/<media_type>/<id>')
 def get(media_type, id):
     result = execute(media_type, id)
     if result == 'success':
-        flash('Download Success')
-        return redirect(url_for('index'))
+        flash('Execute Started')
     elif result == 'no setup':
         flash('Please enter execute instructions in settings')
-    return redirect(url_for('index'))
+    return redirect(request.referrer)
 
 
 @app.route('/settings', methods=["GET", "POST"])
